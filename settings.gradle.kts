eee2f405d4e1192ef75d82583e62113a11349699
BladeFong <lanefeng1989@163.com>
2026-08-18 01:59:02 +0800

    build(deps): settings.gradle.kts 优先引入 mavenLocal 支持本地SDK自适应

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -17,6 +17,7 @@ plugins {
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
+        mavenLocal()
         maven { url = uri("https://maven.aliyun.com/repository/public") }
         maven { url = uri("https://maven.aliyun.com/repository/google") }
         mavenCentral()
